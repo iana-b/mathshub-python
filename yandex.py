191aada81cd0e740984b376f28f6a6dbf494fa9a
iana-b <baraksina.ya@gmail.com>
2023-03-17 21:16:48 +0100

    15/20

--- yandex.py
+++ yandex.py
@@ -11,3 +11,46 @@
 # print(int(m * n * v))
 
 # 10 task:
+# name = input()
+# number = input()
+# print(f"Группа №{number[0]}.\n{number[2]}. {name}.\nШкафчик: {number}.\nКроватка:{number[1]}.")
+
+# name = input()
+# number = input()
+# print(f"Группа №{number[0]}.\n{number[2]}. {name}.\nШкафчик: {number}.\nКроватка: {number[1]}.")
+
+# abcd = input()
+# print(abcd[1], abcd[0], abcd[3], abcd[2], sep="")
+#
+# a = int(input())
+# b = int(input())
+# a_1 = a % 10
+# a_10 = a // 10 % 10
+# a_100 = a // 100
+# b_1 = b % 10
+# b_10 = b // 10 % 10
+# b_100 = b // 100
+# c = (a_100 + b_100) % 10
+# d = (a_10 + b_10) % 10
+# e = (a_1 + b_1) % 10
+# print(c, d, e, sep="")
+
+# children = int(input())
+# candy = int(input())
+# print(candy // children, "\n", candy % children, sep="")
+
+# red = int(input())
+# green = int(input())
+# blue = int(input())
+# print(red + blue + 1)
+
+# n = int(input())  # часов от 0 до 24
+# m = int(input())  # минут от 0 до 60
+# t = int(input())  # минут от 30 до 10**9
+# # n = 10
+# # m = 15
+# # t = 2752
+# hh = (n + (m + t) // 60) % 24
+# mm = (m + t) % 60
+# print(hh // 10, hh % 10, ":", mm // 10, mm % 10, sep="")
+
