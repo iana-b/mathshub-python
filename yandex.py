b45942186057e6ee484c64700620cced7f2cf3d5
Iana <baraksina.ya@gmail.com>
2023-03-17 17:51:48 +0100

    яндекс9

--- yandex.py
+++ yandex.py
@@ -1,6 +1,12 @@
 # a = int(input())
 # print("Купи слона!\n" * a)
 
-n = int(input())
-a = input()
-print(f"Я больше никогда не буду писать \"{a}\"!\n" * n)
+# n = int(input())
+# a = input()
+# print(f"Я больше никогда не буду писать \"{a}\"!\n" * n)
+
+# n = int(input())  # минут
+# m = int(input())  # детей
+# v = 2 / 2 / 2
+# print(int(m * n * v))
+
