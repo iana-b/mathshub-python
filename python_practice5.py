cc9106018c8b88faf6261b449d1c4daa74d0b24f
iana-b <baraksina.ya@gmail.com>
2023-05-17 17:57:34 +0200

    OOP

--- python_practice5.py
+++ python_practice5.py
@@ -268,3 +268,134 @@
 #
 # print(b and a) # print_stat1
 # print(b & a) # print_stat2
+
+# Дополнительная практика
+# Задача 6
+# def is_anagram(first: str, second: str) -> bool:
+#     for letter in first:
+#         if letter in second:
+#             second = second.replace(letter, '', 1)
+#         else:
+#             return False
+#     if second == '':
+#         return True
+#     else:
+#         return False
+#
+#
+# first_word = 'manometrical'
+# second_word = 'commentarial'
+# print(is_anagram(first_word, second_word))
+# print(is_anagram("tastes", "estate"))
+
+# Задача 7
+# 1 способ - через цикл
+# def gcd(a: int, b: int) -> int:
+#   while a != 0 and b != 0:
+#     if a > b:
+#       a = a % b
+#     else:
+#       b = b % a
+#   return a + b
+#
+#
+# number_a = 70
+# number_b = 90
+# print(gcd(number_a, number_b))
+
+# 2 способ - рекурсивный
+# def gcd(a: int, b: int) -> int:
+#   if b == 0:
+#     return a
+#   return gcd(b, a % b)
+#
+#
+#
+# number_a = 70
+# number_b = 90
+# print(gcd(number_a, number_b))
+
+# Задача 8
+# class LifeGame:
+#     def __init__(self, ocean: list):
+#         self.ocean = ocean
+#
+#     def get_next_generation(self):
+#         new_ocean = []
+#         for y in range(len(self.ocean)):
+#             new_ocean.append([])
+#
+#             for x in range(len(self.ocean[y])):
+#                 cell = self.ocean[y][x]
+#                 count = 0
+#                 # для соседей
+#                 for new_y in [y - 1, y, y + 1]:
+#                     for new_x in [x - 1, x, x + 1]:
+#                         if (new_x >= 0 and new_x < len(self.ocean[y])) and (new_y >= 0 and new_y < len(self.ocean)):
+#                             if not (new_x == x and new_y == y):
+#                                 if (self.ocean[new_y][new_x] == 1):
+#                                     count += 1
+#                 if cell == 0:
+#                     if count == 3:
+#                         new_cell = 1
+#                     else:
+#                         new_cell = 0
+#                 elif cell == 1:
+#                     if count == 2 or count == 3:
+#                         new_cell = 1
+#                     else:
+#                         new_cell = 0
+#                 new_ocean[y].append(new_cell)
+#
+#         self.ocean = new_ocean
+#         return self
+#
+#     def __str__(self) -> str:
+#         new_str = ''
+#         for lst in self.ocean:
+#             for x in lst:
+#                 if x == 0:
+#                     new_str += '•'
+#                 elif x == 1:
+#                     new_str += '■'
+#                 new_str += ' '
+#             new_str += '\n'
+#         return new_str
+#
+#     def __repr__(self) -> str:
+#         pass
+#
+#
+# my_ocean = [
+#     [0, 0, 0, 0, 0],
+#     [0, 0, 1, 0, 0],
+#     [0, 0, 1, 0, 0],
+#     [0, 0, 1, 0, 0],
+#     [0, 0, 0, 0, 0]
+# ]
+#
+# # my_ocean = [
+# #     [0, 0, 0, 0, 0],
+# #     [0, 0, 0, 1, 0],
+# #     [0, 1, 0, 1, 0],
+# #     [0, 0, 1, 1, 0],
+# #     [0, 0, 0, 0, 0]
+# # ]
+# life_game = LifeGame(my_ocean)
+# print(life_game)
+#
+# GameOn = True
+# prev_ocean = []
+# while GameOn:
+#     prev_ocean.append(life_game.ocean)
+#     print(life_game.get_next_generation())
+#     count = 0
+#     for y in range(len(life_game.ocean)):
+#         for x in range(len(life_game.ocean[y])):
+#             if x != 0:
+#                 count += 1
+#     if count == 0:
+#         GameOn = False
+#     for ocean in prev_ocean:
+#         if life_game.ocean == ocean:
+#             GameOn = False
